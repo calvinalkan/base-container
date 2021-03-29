45c520155fbf660815178f17b0aac56a60f8f9d8
calvinalkan <calvin@snicco.de>
2021-03-28 22:13:45 -0600

    Initial commit

---
+++ BaseContainerAdapter.php
@@ -0,0 +1,97 @@
+<?php
+	
+	namespace SniccoAdapter;
+	
+	use Contracts\SniccoContainerAdapter;
+	use Illuminate\Container\Container;
+	use Illuminate\Contracts\Container\Container as IlluminateContainer;
+	
+	class BaseContainerAdapter implements SniccoContainerAdapter {
+		
+		/** @var Container */
+		private $container;
+		
+		private string $name = 'illuminate_container';
+		
+		public function __construct( IlluminateContainer $container = NULL ) {
+			
+			$this->container = $container ?? new Container();
+		}
+		
+		
+		public function offsetExists( $offset ) {
+			
+			return $this->container->bound( $offset );
+			
+		}
+		
+		public function offsetGet( $offset ) {
+			
+			return $this->container->make( $offset );
+			
+		}
+		
+		public function offsetSet( $offset, $value ) {
+			
+			// Wrap primitives in a closure.
+			if ( ! $value instanceof \Closure && ! is_object( $value ) ) {
+				
+				$value = function () use ( $value ) {
+					return $value;
+				};
+				
+			}
+			
+			if ( $value instanceof \Closure ) {
+				
+				
+				$this->container->singleton( $offset, $value );
+				
+				return;
+				
+				
+			}
+			
+			if ( is_object( $value ) ) {
+				
+				$this->container->instance( $offset, $value );
+				
+				return;
+				
+			}
+			
+			
+		}
+		
+		public function offsetUnset( $offset ) {
+			
+			unset( $this->container->bindings[ $offset ], $this->instances[ $offset ], $this->resolved[ $offset ] );
+			
+			
+		}
+		
+		public function make( $abstract, array $parameters = [] ): object {
+			
+			return $this->container->make( $abstract, $parameters );
+			
+		}
+		
+		public function swapInstance( $abstract, $concrete ) {
+			
+			$this->instance( $abstract, $concrete );
+			
+		}
+		
+		public function instance( $abstract, $instance ) {
+			
+			$this->container->instance( $abstract, $instance );
+			
+		}
+		
+		public function call( $callable, array $parameters = [] ) {
+			
+			return $this->container->call( $callable, $parameters);
+			
+		}
+		
+	}
